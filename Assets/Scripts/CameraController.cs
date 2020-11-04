7abf3631a39a48f21c85ec737b179f2fd76f7eeb
Dylan Sleith <sleithdylan@gmail.com>
2020-11-04 21:17:42 +0000

    Fix movement when camera rotates

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,6 +7,7 @@ public class CameraController : MonoBehaviour
     // Public variables
     public GameObject player;
     private Vector3 offset;
+    public float turnSensitivity = 100;
 
     // Start is called before the first frame update
     void Start()
@@ -19,5 +20,18 @@ public class CameraController : MonoBehaviour
     void LateUpdate()
     {
         transform.position = player.transform.position + offset;
+
+        // If user presses "E" or "Mouse Button 2", rotate camera right
+        if (Input.GetKey(KeyCode.E) || Input.GetMouseButton(1))
+        {
+            transform.RotateAround(player.transform.position, Vector3.up, turnSensitivity * Time.deltaTime);
+            offset = transform.position - player.transform.position;
+        }
+        // If user presses "Q" or "Mouse Button 1", rotate camera left
+        else if (Input.GetKey(KeyCode.Q) || Input.GetMouseButton(0))
+        {
+            transform.RotateAround(player.transform.position, Vector3.up, -turnSensitivity * Time.deltaTime);
+            offset = transform.position - player.transform.position;
+        }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,11 +7,15 @@ public class PlayerController : MonoBehaviour
 {
     // Public variables
     public float speed = 0;
+    public Transform CameraPivot;
+    public Transform cam;
 
     // Private variables
     private Rigidbody rb;
     private float movementX;
     private float movementY;
+
+    Vector2 input;
 
     // Start is called before the first frame update
     void Start()
@@ -25,11 +29,31 @@ public class PlayerController : MonoBehaviour
         // Gets "Vector2" data from movementValue
         Vector2 movementVector = movementValue.Get<Vector2>();
 
+        // X movementValue
         movementX = movementVector.x;
+        // Y movementValue
         movementY = movementVector.y;
     }
 
-    // For physics
+    void Update()
+    {
+        // Get X & Y Axis
+        input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        input = Vector2.ClampMagnitude(input, 1);
+
+        Vector3 camForward = cam.forward;
+        Vector3 camRight = cam.right;
+
+        camForward.y = 0;
+        camRight.y = 0;
+        camForward = camForward.normalized;
+        camRight = camRight.normalized;
+
+        // Adds movement
+        transform.position += (camForward * input.y + camRight * input.x) * Time.deltaTime * 1.5f;
+    }
+
+    // Physics
     void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
