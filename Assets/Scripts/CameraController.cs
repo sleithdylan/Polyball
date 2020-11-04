97669b2f2fa19fd04d6701bd6ea1f61abf55800f
Dylan Sleith <sleithdylan@gmail.com>
2020-11-04 01:08:52 +0000

    Fix player camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Public variables
+    public GameObject player;
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Camera Transform Position - Player Transform Position
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame but runs when all other updates are done
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
